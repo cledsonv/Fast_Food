437a2d288ad926e6c26cbea20097b5266164c832
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2024-08-11 13:18:04 -0300

    feat readmine

---
+++ README.md
@@ -0,0 +1,60 @@
+# Fast Food App
+
+## Descrição
+O **Fast Food App** é uma aplicação móvel desenvolvida em Kotlin utilizando o framework Jetpack Compose. A aplicação permite aos usuários visualizar e fazer pedidos de diversos tipos de alimentos, como sorvetes, pizzas, carnes de porco, sanduíches, salsichas, bifes e outros.
+
+## Imagens
+
+![Todos os Pedidos](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/all_order.jpg)
+![Categoria](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/category_page.jpg)
+![Deletar pedido](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/delete_order.jpg)
+
+![Finalizar Pedido](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/finish_order.jpg)
+![Tela Inicial](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/foods_page.jpg)
+![Selecionar Pedido](https://github.com/cledsonv/Fast_Food/tree/master/app/src/main/res/assets/select_order.jpg)
+
+
+## Funcionalidades
+- Visualização de diferentes categorias de alimentos.
+- Detalhamento de cada alimento com imagem, descrição, preço e avaliação.
+- Adição de itens ao pedido com seleção de quantidade.
+- Finalização de pedidos.
+- Armazenamento de pedidos no banco de dados local.
+- Pesquisa de alimentos.
+
+## Tecnologias Utilizadas
+- **Linguagem:** Kotlin
+- **Framework:** Jetpack Compose
+- **Requisição de API:** Retrofit
+- **Gerenciamento de Dependências:** Hilt
+- **Arquitetura:** MVVM (Model-View-ViewModel)
+- **Banco de Dados:** Room
+- **Design de Interface:** Atomic Design para organização das pastas.
+
+## Estrutura do Projeto
+- `app/src/main/java/com/codeventura/fast_food/`
+    - `data/`:
+        - `api/`: Contém as rotas e APIs.
+        - `dao/`: Contém as interfaces para o banco de dados.
+        - `database/`: Contém a configuração do banco de dados Room e DAOs.
+        - `mappers/`: Contém os conversores `fromJson` e `toJson`.
+        - `model/`: Contém as classes de dados (e.g., `FoodItem.kt`).
+        - `repository/`: Contém os repositórios para acesso aos dados.
+    - `ui/`:
+        - `atomic/`: Contém componentes simples e reutilizáveis, como `NetworkImage` e `Rating`.
+        - `molecules/`: Agrupa átomos para criar componentes principais.
+        - `organisms/`: Combina moléculas para formar listas e outras estruturas.
+        - `pages/`: Contém as telas principais da aplicação (e.g., `FoodPage.kt`, `OrderPage.kt`).
+        - `themes/`: Contém as cores e temas do aplicativo.
+    - `viewmodel/`:
+        - Contém os ViewModels que gerenciam a lógica de negócios e a comunicação com os repositórios de dados.
+
+## Como Executar
+1. Clone o repositório:
+   ```sh
+   git clone https://github.com/cledsonv/Fast_Food.git
+   ```
+2. Abra o projeto no Android Studio.
+3. Construa e execute o projeto no emulador ou em um dispositivo físico.
+
+
--- app/src/main/java/com/codeventura/fast_food/viewmodel/FoodViewModel.kt
+++ app/src/main/java/com/codeventura/fast_food/viewmodel/FoodViewModel.kt
@@ -2,6 +2,8 @@ package com.codeventura.fast_food.viewmodel
 
 import android.app.Application
 import android.util.Log
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -33,24 +35,86 @@ class FoodViewModel @Inject constructor(
 	val filteredFoodItems: LiveData<List<FoodModel>> get() = _filteredFoodItems
 	private val _filteredOrderItems = MutableLiveData<List<OrderItem>>()
 	val filteredOrderItems: LiveData<List<OrderItem>> get() = _filteredOrderItems
+	private val _errorMessage = mutableStateOf<String?>(null)
+	val errorMessage: State<String?> = _errorMessage
 	
 	
 	val foodRoutes = listOf(
-		FoodRoute(name = "Churrasco", route = "bbqs", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl0zPT9JnZjHZCKFFHzIPbe0wS7MlwE568EQ&s"),
-		FoodRoute(name = "Melhores Alimentos", route = "best-foods", image = "https://wx.mlcdn.com.br/ponzi/production/portaldalu/43344_01.jpg"),
-		FoodRoute(name = "Pães", route = "breads", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGBjevCIICr24E66wMO9CWPSHathASA35R8Q&s"),
-		FoodRoute(name = "Hambúrgueres", route = "burgers", image = "https://www.sabornamesa.com.br/media/k2/items/cache/bf1e20a4462b71e3cc4cece2a8c96ac8_XL.jpg"),
-		FoodRoute(name = "Chocolates", route = "chocolates", image = "https://ecp.yusercontent.com/mail?url=https%3A%2F%2Fcseprs3.s3.amazonaws.com%2Femail-editor-files%2Faee2f952-3231-436d-a46e-08d9f0b2efaa%2F7cb62c07-dd0b-4932-a21e-107ea18283b8.jpg&t=1720194785&ymreqid=3ea9396a-53ac-76b5-1c06-b70008010500&sig=eDz4lnJyH6Htraf2J7zCQA--~D"),
-		FoodRoute(name = "Sobremesas", route = "desserts", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXW098aMgh0oLDONxBtWs2ff1DHYS2gYo6HA&s"),
-		FoodRoute(name = "Bebidas", route = "drinks", image = "https://conteudo.imguol.com.br/c/entretenimento/74/2022/09/13/drinques-bebida-alcoolica-tequila-cerveja-chopp-gim-martini-alcool-copos-tacas-1663094165597_v2_4x3.jpg"),
-		FoodRoute(name = "Frango Frito", route = "fried-chicken", image = "https://pubimg.band.uol.com.br/files/814f09873d737cbc500d.png"),
-		FoodRoute(name = "Sorvetes", route = "ice-cream", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp98KIc66cgKAPeOwIBnqKb9xeoHIFXf8wMA&s"),
-		FoodRoute(name = "Pizzas", route = "pizzas", image = "https://istoe.com.br/wp-content/uploads/2023/07/feche-a-pizza-italiana-sobre-o-queijo-cole-o-foco-seletivo-generativo-ai-1258-153047.jpeg"),
-		FoodRoute(name = "Carnes de Porco", route = "porks", image = "https://www.juliatto.com.br/wp-content/uploads/2023/05/preparar-carne-de-porco-de-forma-saudavel.jpg"),
-		FoodRoute(name = "Sanduíches", route = "sandwiches", image = "https://www.unochapeco.edu.br/static/data/portal/noticias/fotos/960x960/9879.jpg"),
-		FoodRoute(name = "Salsichas", route = "sausages", image = "https://conteudo.imguol.com.br/c/entretenimento/0c/2018/09/18/tipos-de-linguicas-1537301783500_v2_900x506.jpg"),
-		FoodRoute(name = "Bifes", route = "steaks", image = "https://www.pingodoce.pt/wp-content/uploads/2016/06/bifes-angus-com-presunto.jpg"),
-		FoodRoute(name = "Nossos Alimentos", route = "our-foods", image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfQtp6m__wPVhZ4Jv99xneEo4ESOTX9WbZGG0r2dNTqeBdMWFuxdbM7FGiMs-PmnY4QIs&usqp=CAU")
+		FoodRoute(
+			name = "Churrasco",
+			route = "bbqs",
+			image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl0zPT9JnZjHZCKFFHzIPbe0wS7MlwE568EQ&s"
+		),
+		FoodRoute(
+			name = "Melhores Alimentos",
+			route = "best-foods",
+			image = "https://wx.mlcdn.com.br/ponzi/production/portaldalu/43344_01.jpg"
+		),
+		FoodRoute(
+			name = "Pães",
+			route = "breads",
+			image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGBjevCIICr24E66wMO9CWPSHathASA35R8Q&s"
+		),
+		FoodRoute(
+			name = "Hambúrgueres",
+			route = "burgers",
+			image = "https://www.sabornamesa.com.br/media/k2/items/cache/bf1e20a4462b71e3cc4cece2a8c96ac8_XL.jpg"
+		),
+		FoodRoute(
+			name = "Chocolates",
+			route = "chocolates",
+			image = "https://ecp.yusercontent.com/mail?url=https%3A%2F%2Fcseprs3.s3.amazonaws.com%2Femail-editor-files%2Faee2f952-3231-436d-a46e-08d9f0b2efaa%2F7cb62c07-dd0b-4932-a21e-107ea18283b8.jpg&t=1720194785&ymreqid=3ea9396a-53ac-76b5-1c06-b70008010500&sig=eDz4lnJyH6Htraf2J7zCQA--~D"
+		),
+		FoodRoute(
+			name = "Sobremesas",
+			route = "desserts",
+			image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXW098aMgh0oLDONxBtWs2ff1DHYS2gYo6HA&s"
+		),
+		FoodRoute(
+			name = "Bebidas",
+			route = "drinks",
+			image = "https://conteudo.imguol.com.br/c/entretenimento/74/2022/09/13/drinques-bebida-alcoolica-tequila-cerveja-chopp-gim-martini-alcool-copos-tacas-1663094165597_v2_4x3.jpg"
+		),
+		FoodRoute(
+			name = "Frango Frito",
+			route = "fried-chicken",
+			image = "https://pubimg.band.uol.com.br/files/814f09873d737cbc500d.png"
+		),
+		FoodRoute(
+			name = "Sorvetes",
+			route = "ice-cream",
+			image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp98KIc66cgKAPeOwIBnqKb9xeoHIFXf8wMA&s"
+		),
+		FoodRoute(
+			name = "Pizzas",
+			route = "pizzas",
+			image = "https://istoe.com.br/wp-content/uploads/2023/07/feche-a-pizza-italiana-sobre-o-queijo-cole-o-foco-seletivo-generativo-ai-1258-153047.jpeg"
+		),
+		FoodRoute(
+			name = "Carnes de Porco",
+			route = "porks",
+			image = "https://www.juliatto.com.br/wp-content/uploads/2023/05/preparar-carne-de-porco-de-forma-saudavel.jpg"
+		),
+		FoodRoute(
+			name = "Sanduíches",
+			route = "sandwiches",
+			image = "https://www.unochapeco.edu.br/static/data/portal/noticias/fotos/960x960/9879.jpg"
+		),
+		FoodRoute(
+			name = "Salsichas",
+			route = "sausages",
+			image = "https://conteudo.imguol.com.br/c/entretenimento/0c/2018/09/18/tipos-de-linguicas-1537301783500_v2_900x506.jpg"
+		),
+		FoodRoute(
+			name = "Bifes",
+			route = "steaks",
+			image = "https://www.pingodoce.pt/wp-content/uploads/2016/06/bifes-angus-com-presunto.jpg"
+		),
+		FoodRoute(
+			name = "Nossos Alimentos",
+			route = "our-foods",
+			image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfQtp6m__wPVhZ4Jv99xneEo4ESOTX9WbZGG0r2dNTqeBdMWFuxdbM7FGiMs-PmnY4QIs&usqp=CAU"
+		)
 	)
 	
 	init {
@@ -60,14 +124,22 @@ class FoodViewModel @Inject constructor(
 	}
 	
 	fun fetchFoodItems(route: String) {
-		viewModelScope.launch {
-			repository.getFoodItems(route = route) { items ->
-				_foodItems.postValue(items ?: emptyList())
-				_filteredFoodItems.postValue(items ?: emptyList())
+		try {
+			viewModelScope.launch {
+				repository.getFoodItems(route = route) { items ->
+					_foodItems.postValue(items ?: emptyList())
+					_filteredFoodItems.postValue(items ?: emptyList())
+				}
 			}
+		} catch (e: Exception) {
+			_errorMessage.value = e.message
 		}
 	}
 	
+	fun clearErrorMessage(route: String) {
+		_errorMessage.value = null
+		fetchFoodItems(route)
+	}
 	
 	fun filterFoodItems(query: String) {
 		_filteredFoodItems.value = _foodItems.value?.filter {
@@ -78,7 +150,7 @@ class FoodViewModel @Inject constructor(
 	fun getFoodItemById(id: String, route: String): MutableLiveData<FoodModel?> {
 		val foodItem = MutableLiveData<FoodModel?>()
 		viewModelScope.launch {
-			repository.getFoodItemById(id =id, route= route) { item ->
+			repository.getFoodItemById(id = id, route = route) { item ->
 				foodItem.postValue(item)
 			}
 		}
@@ -93,10 +165,8 @@ class FoodViewModel @Inject constructor(
 		viewModelScope.launch(Dispatchers.IO) {
 			try {
 				orderItemDao.insert(orderItem)
-				// Log ou outra forma de feedback para confirmar que a inserção foi realizada
 				Log.d("OrderViewModel", "Pedido inserido com sucesso: $orderItem")
 			} catch (e: Exception) {
-				// Lidar com o erro, por exemplo, registrar ou mostrar uma mensagem de erro
 				Log.e("OrderViewModel", "Erro ao inserir pedido", e)
 			}
 		}
@@ -110,7 +180,6 @@ class FoodViewModel @Inject constructor(
 				_filteredOrderItems.postValue(orders)
 				Log.d("OrderViewModel", "Pedidos obtidos: $orders")
 			} catch (e: Exception) {
-				// Lidar com o erro, por exemplo, registrar ou mostrar uma mensagem de erro
 				Log.e("OrderViewModel", "Erro ao buscar pedidos", e)
 			}
 		}
@@ -121,10 +190,8 @@ class FoodViewModel @Inject constructor(
 			try {
 				orderItemDao.deleteById(id)
 				getOrders()
-				// Log ou outra forma de feedback para confirmar que a exclusão foi realizada
 				Log.d("OrderViewModel", "Pedido excluído com sucesso: $id")
 			} catch (e: Exception) {
-				// Lidar com o erro, por exemplo, registrar ou mostrar uma mensagem de erro
 				Log.e("OrderViewModel", "Erro ao excluir pedido", e)
 			}
 		}
@@ -136,5 +203,5 @@ class FoodViewModel @Inject constructor(
 			it.food.name.contains(query, ignoreCase = true)
 		}
 	}
-
+	
 }
